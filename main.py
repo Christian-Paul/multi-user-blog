a1994ebb26a08b33b0447c8493dbce8f0f9142b8
Christian <christiansamuelpaul@gmail.com>
2017-03-12 16:23:53 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+
+import webapp2
+import logging
+import jinja2
+import re
+import time
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Post(db.Model):
+  subject = db.StringProperty(required = True)
+  content = db.TextProperty(required = True)
+  created = db.DateTimeProperty(auto_now_add = True)
+  author = db.StringProperty(default = 'Anonymous')
+
+class Handler(webapp2.RequestHandler):
+  def write(self, *a, **kw):
+    self.response.out.write(*a, **kw)
+
+  def render_str(self, template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+  def render(self, template, **kw):
+    self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+  def get(self):
+    posts = db.GqlQuery('SELECT * FROM Post ORDER BY created DESC')
+
+    self.render('index.html', posts = posts)
+
+class PostHandler(Handler):
+  def get(self, post_id):
+    post = Post.get_by_id(int(post_id))
+
+    if post:
+      self.render('post.html', post = post)
+    else:
+      self.error(404)
+
+class NewPost(Handler):
+  def get(self):
+    self.render('new-post.html')
+
+  def post(self):
+    subject = self.request.get('subject')
+    content = self.request.get('content')
+
+    params = dict(subject = subject,
+                  content = content)
+
+    if subject and content and len(content) > 250:
+      p = Post(subject = subject, content = content)
+      p.put()
+      post_id = str(p.key().id())
+
+      time.sleep(0.1)
+
+      self.redirect('/post/' + post_id)
+    else:
+      params['error_status'] = True
+
+      if len(content) <= 250:
+        params['error_message'] = 'Post content must contain more than 250 characters'
+      else:
+        params['error_message'] = 'An error occurred'
+
+      self.render('new-post.html', **params)
+
+
+app = webapp2.WSGIApplication([('/', MainPage), 
+                              ('/newpost', NewPost),
+                              ('/post/(\d+)', PostHandler)
+                              ],
+                              debug=True)
